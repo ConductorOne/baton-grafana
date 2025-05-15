8846bf5ffb4a24b95b4b1a7ecd19741b9866d22c
MB Burch <mb.burch@conductorone.com>
2025-05-15 12:08:39 -0600

    WIP

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,4 +1,4 @@
-package main
+package config
 
 import (
 	"github.com/conductorone/baton-sdk/pkg/field"
@@ -9,27 +9,21 @@ var (
 	Hostname = field.StringField("hostname", field.WithDescription("The Grafana hostname used to connect to the Grafana API"), field.WithDefaultValue("http://localhost:3000"))
 	Username = field.StringField("username", field.WithRequired(true), field.WithDescription("The Grafana username used to connect to the Grafana API."))
 	Password = field.StringField("password", field.WithRequired(true), field.WithDescription("The Grafana password used to connect to the Grafana API."))
-	// ConfigurationFields defines the external configuration required for the
-	// connector to run. Note: these fields can be marked as optional or
-	// required.
-	ConfigurationFields = []field.SchemaField{
-		Hostname,
-		Username,
-		Password,
-	}
 
 	// FieldRelationships defines relationships between the fields listed in
-	// ConfigurationFields that can be automatically validated. For example, a
+	// Config that can be automatically validated. For example, a
 	// username and password can be required together, or an access token can be
 	// marked as mutually exclusive from the username password pair.
 	FieldRelationships = []field.SchemaFieldRelationship{
 		field.FieldsRequiredTogether(Username, Password),
 	}
 
-	cfg = field.Configuration{
-		Fields:      ConfigurationFields,
-		Constraints: FieldRelationships,
-	}
+	//go:generate go run ./gen
+	Config = field.NewConfiguration([]field.SchemaField{
+		Hostname,
+		Username,
+		Password,
+	}, FieldRelationships...)
 )
 
 // ValidateConfig is run after the configuration is loaded, and should return an
